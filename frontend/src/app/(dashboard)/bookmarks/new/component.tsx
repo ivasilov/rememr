ed31dbccff2724146df835666f6f438275310f3a
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-02-18 22:51:43 +0100

    Rewrite the page for new bookmark.

--- frontend/src/app/(dashboard)/bookmarks/new/component.tsx
+++ frontend/src/app/(dashboard)/bookmarks/new/component.tsx
@@ -1,50 +1,154 @@
 'use client';
-import { EditPagesForBookmark, IdName } from '@/src/components/edit-pages-for-bookmark';
-import { FormGroup } from '@/src/components/form-group';
-import { Button } from '@/src/components/ui/button';
-import { Input } from '@/src/components/ui/input';
-import { uniqBy } from 'lodash';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useState } from 'react';
-import { save } from './action';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { EditPagesForBookmark } from '../../../../components/edit-pages-for-bookmark';
+import { Button } from '../../../../components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../../../../components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../../../../components/ui/form';
+import { Input } from '../../../../components/ui/input';
+import { Switch } from '../../../../components/ui/switch';
+import { createClient } from '../../../../utils/supabase/client';
+
+const formId = 'create-new-bookmark';
+
+const NewBookmarkSchema = z.object({
+  name: z.string(),
+  url: z.string(),
+  read: z.boolean(),
+  tagIds: z.array(
+    z.object({
+      id: z.string(),
+      name: z.string(),
+    }),
+  ),
+});
 
 export const NewBookmarkComponent = () => {
+  const supabase = createClient();
+
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const [name, setName] = useState(searchParams.get('title') || '');
-  const [url, setUrl] = useState(searchParams.get('url') || '');
-  const [pages, setPages] = useState<IdName[]>([]);
+  const form = useForm<z.infer<typeof NewBookmarkSchema>>({
+    resolver: zodResolver(NewBookmarkSchema),
+    defaultValues: {
+      name: searchParams.get('title') || '',
+      url: searchParams.get('url') || '',
+    },
+  });
 
-  const changeName = (e: React.FormEvent<HTMLInputElement>) => {
-    setName(e.currentTarget.value);
-  };
+  const onSubmit: SubmitHandler<z.infer<typeof NewBookmarkSchema>> = async values => {
+    const tagNames = values.tagIds.filter(t => !t.id).map(t => ({ name: t.name }));
+    const { data: newTags } = await supabase.from('tags').insert(tagNames).select();
 
-  const changeUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUrl(e.currentTarget.value);
-  };
+    const { data, error } = await supabase
+      .from('bookmarks')
+      .insert({
+        name: values.name,
+        url: values.url,
+        read: values.read,
+      })
+      .select();
 
-  const changePages = (p: IdName[]) => {
-    console.log(p);
-    let pages = p.map(p => ({ id: p.id, name: p.name }));
-    setPages(uniqBy(pages, p => p.name));
+    if (error || data.length !== 1) {
+      // TODO: handle this case
+      return;
+    }
+    const bookmark = data[0];
+
+    const existingTags = values.tagIds.filter(t => t.id);
+    const relations = [...(newTags || []), ...existingTags].map(r => ({ tag_id: r.id!, bookmark_id: bookmark.id }));
+
+    await supabase.from('bookmarks_tags').delete().eq('bookmark_id', bookmark.id);
+    if (relations.length > 0) {
+      await supabase.from('bookmarks_tags').insert(relations);
+    }
   };
 
   return (
-    <div className="w-5/6 mx-auto mt-8">
-      <FormGroup htmlFor="name" label="Name of the bookmark">
-        <Input onChange={changeName} value={name} />
-      </FormGroup>
-      <FormGroup htmlFor="url" label="Url of the bookmark">
-        <Input onChange={changeUrl} value={url} />
-      </FormGroup>
-      <FormGroup htmlFor="pages-select" label="Pages which have this bookmark">
-        <EditPagesForBookmark pages={pages} onChange={changePages} />
-      </FormGroup>
-      <div>
-        <Button onClick={() => router.push('/')}>Cancel</Button>
-        <Button onClick={() => save(name, url, pages)}>Save</Button>
-      </div>
-    </div>
+    <Card className="w-full max-w-xl">
+      <CardHeader>
+        <CardTitle>New bookmark</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form id={formId} onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name of the bookmark</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="url"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Url of the bookmark</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="read"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+                  <div className="space-y-0.5">
+                    <FormLabel>Read</FormLabel>
+                    <FormDescription>If marked as unread, it will show up in the unread list.</FormDescription>
+                  </div>
+                  <FormControl>
+                    <Switch checked={field.value} onCheckedChange={field.onChange} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="tagIds"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tags which have this bookmark</FormLabel>
+                  <FormControl>
+                    <EditPagesForBookmark pages={field.value} onChange={ids => field.onChange(ids)} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter className="flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2">
+        <Button variant="secondary" onClick={() => router.push('/')}>
+          Cancel
+        </Button>
+        <Button variant="default" type="submit" form={formId}>
+          Save
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
--- frontend/src/app/(dashboard)/bookmarks/new/page.tsx
+++ frontend/src/app/(dashboard)/bookmarks/new/page.tsx
@@ -5,7 +5,11 @@ import { NewBookmarkComponent } from './component';
 const NewBookmarkPage = async () => {
   await checkAuthentication();
 
-  return <NewBookmarkComponent />;
+  return (
+    <div className="flex justify-center pt-8">
+      <NewBookmarkComponent />
+    </div>
+  );
 };
 
 export default NewBookmarkPage;
