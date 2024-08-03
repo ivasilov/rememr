e971e744d5e33e2afedf927a225894d921cbd78b
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-08-04 00:56:07 +0200

    Add sonner for toasts.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
+import { Toaster } from '../components/ui/sonner'
 import './globals.css'
 
 export const metadata = {
@@ -13,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html className="h-full bg-gray-100">
-      <body className="font-sans">{children}</body>
+      <body className="font-sans">
+        {children}
+        <Toaster position="top-right" />
+      </body>
     </html>
   )
 }
