485c8bc443320b77b43b7d608ac25b5901a4ccbd
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-10-09 23:22:23 +0200

    Add command component. Reorder imports in main layout.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,15 +1,14 @@
 // add fontawesome CSS so that the icons are don't cause layout shift
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-
 import { Toaster } from '@rememr/ui'
-
+import { ReactNode } from 'react'
 import { Providers } from './providers'
 
 import '@rememr/ui/globals.css'
-import { ReactNode } from 'react'
 import './globals.css'
+
+config.autoAddCss = false
 
 export const metadata = {
   title: 'rememr',
