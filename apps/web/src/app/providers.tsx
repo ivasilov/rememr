f379a8e297e8f8715a8a8d2caff90eb922eef2a4
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-10-27 14:19:11 +0100

    Add react-query and nuqs libs.

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,17 +1,24 @@
 'use client'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import { PropsWithChildren } from 'react'
+import { getQueryClient } from '../lib/react-query-client'
 
 export const Providers = ({ children }: PropsWithChildren<{}>) => {
+  const queryClient = getQueryClient()
+
   return (
-    <ThemeProvider
-      attribute="class"
-      enableSystem
-      themes={['light', 'dark']}
-      defaultTheme="light"
-      storageKey="rememr-theme-key"
-    >
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        enableSystem
+        themes={['light', 'dark']}
+        defaultTheme="light"
+        storageKey="rememr-theme-key"
+      >
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
