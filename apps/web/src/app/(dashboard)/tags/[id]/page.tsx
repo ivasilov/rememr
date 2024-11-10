74b97b56e48c6173f0ee276f63f6afed97ccc115
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-11-10 14:18:36 +0100

    Fix the tag page, add delete button to it.

--- apps/web/src/app/(dashboard)/tags/[id]/page.tsx
+++ apps/web/src/app/(dashboard)/tags/[id]/page.tsx
@@ -1,9 +1,13 @@
 'use server'
+import { Bookmarks } from '@/src/components/bookmarks'
+import { Icon } from '@/src/components/icon'
 import { checkAuthentication } from '@/src/lib/supabase'
 import { createClient } from '@/src/utils/supabase/server'
+import { faTag } from '@fortawesome/free-solid-svg-icons'
 import { cookies } from 'next/headers'
+import page from '../../bookmarks/page'
 import { SinglePageError } from './components/error'
-import { ReadOnlyPage } from './components/read-only-page'
+import { TagActions } from './tag-actions'
 
 const TagPage = async ({ params: { id } }: { params: { id: string } }) => {
   await checkAuthentication(`/tags/${id}`)
@@ -17,7 +21,18 @@ const TagPage = async ({ params: { id } }: { params: { id: string } }) => {
   }
 
   if (tag) {
-    return <ReadOnlyPage page={tag} />
+    return (
+      <div className="container flex h-full flex-col pb-4">
+        <div className="flex items-center py-4 pl-7">
+          <Icon name={faTag} size="1x" className="pr-2" />
+          <div className="flex-grow">
+            <h1 className="text-foreground text-3xl font-semibold">{page.name}</h1>
+          </div>
+          <TagActions tag={tag} />
+        </div>
+        <Bookmarks tags={[tag.id]} />
+      </div>
+    )
   }
 
   return <SinglePageError />
