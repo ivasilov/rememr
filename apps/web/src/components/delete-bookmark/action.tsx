1887ae84d4feaa9ce9fc8ab2681543a890c01a7e
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-11-30 14:10:12 +0100

    Fix the delete bookmark action.

--- apps/web/src/components/delete-bookmark/action.tsx
+++ apps/web/src/components/delete-bookmark/action.tsx
@@ -7,7 +7,8 @@ export const deleteBookmark = async (id: string) => {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
 
-  await supabase.from('bookmarks').delete().eq('id', id)
+  await supabase.from('bookmarks_tags').delete().eq('bookmark_id', id).throwOnError()
+  await supabase.from('bookmarks').delete().eq('id', id).throwOnError()
 
-  revalidatePath('/bookmarks')
+  revalidatePath('/bookmarks', 'page')
 }
--- apps/web/src/components/delete-bookmark/index.tsx
+++ apps/web/src/components/delete-bookmark/index.tsx
@@ -1,4 +1,5 @@
 import { Button, DialogFooter, DialogHeader, DialogTitle } from '@rememr/ui'
+import { toast } from 'sonner'
 import { deleteBookmark } from './action'
 
 interface Props {
@@ -7,19 +8,38 @@ interface Props {
 }
 
 export const DeleteBookmarkDialog = ({ bookmark, onClose }: Props) => {
+  const onDelete = async () => {
+    try {
+      await deleteBookmark(bookmark.id)
+      toast.success(
+        <span>
+          Succesfully deleted <span className="text-destructive">{bookmark.name}</span>.
+        </span>,
+      )
+      onClose()
+    } catch (e: any) {
+      console.log(e)
+      toast.error(
+        <span>
+          Error happened while trying to delete a bookmark: <span className="text-destructive">{e?.message}</span>.
+        </span>,
+      )
+    }
+  }
+
   return (
     <>
       <DialogHeader>
         <DialogTitle>Delete a bookmark?</DialogTitle>
       </DialogHeader>
       <div>
-        Are you sure you want to delete <span style={{ color: 'orange' }}>{bookmark.name}</span>?
+        Are you sure you want to delete <span className="text-destructive">{bookmark.name}</span>?
       </div>
       <DialogFooter>
         <Button variant="secondary" onClick={onClose}>
           Cancel
         </Button>
-        <Button variant="destructive" onClick={() => deleteBookmark(bookmark.id)}>
+        <Button variant="destructive" onClick={onDelete}>
           Delete
         </Button>
       </DialogFooter>
