51efa67430fdae520f3765f93caecd81c67b7968
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-05-09 19:27:05 +0200

    Reimport all shadcn components to match the new tailwind version.

--- packages/ui/src/components/skeleton.tsx
+++ packages/ui/src/components/skeleton.tsx
@@ -1,7 +1,7 @@
-import { cn } from '../utils'
+import { cn } from '@/utils'
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('bg-primary/10 animate-pulse rounded-md', className)} {...props} />
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
+  return <div data-slot="skeleton" className={cn('bg-accent animate-pulse rounded-md', className)} {...props} />
 }
 
 export { Skeleton }
