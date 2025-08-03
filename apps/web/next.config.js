3ad2ded02d6ac09414975842701733592c539e03
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-08-03 15:32:31 +0200

    Remove the landing page from the web app.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: () => [
+    {
+      source: '/',
+      destination: '/bookmarks',
+      permanent: false,
+    },
+  ],
   transpilePackages: ['@rememr/ui'],
 }
 
